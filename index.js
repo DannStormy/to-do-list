0c38486378b4177e9679a8c2bf8d3d94f35e2178
DannStormy <dillydot22@gmail.com>
2022-08-10 23:31:39 +0100

    80 working

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-(function(){
+(function () {
   document.addEventListener('DOMContentLoaded', () => {
     let button = document.querySelector(".button");
     let addTask = document.querySelector(".add-task");
     let lists = document.querySelector(".ul-lists");
-
+    let whichTodos = document.querySelector(".check-todos");
 
     let listItems = [];
 
@@ -13,32 +13,13 @@
         checked: false,
         id: Date.now()
       };
-
       listItems.push(todo);
-      localStorage.setItem("todos", JSON.stringify(listItems));
-      console.log(listItems);
+      // local(listItems)
       showTodo(todo)
     }
 
-    lists.addEventListener('click', (event) =>{
-      const item = event.target;
-      if(item.classList[0] === 'delete'){
-        const todo = item.parentElement;
-        todo.classList.add('fall');
-        todo.addEventListener('transitionend', () => {
-          todo.remove()
-        })
-        // todo.remove()
-      }
-      if(item.classList[0] === 'list-marker-incomplete'){
-        const todo = item.parentElement;
-        item.classList.toggle('list-marker')
-        todo.classList.toggle('show-completed')
-      }
-  })
-
-    function showTodo(todo){
-      const checked = todo.checked ? 'done': '';
+    function showTodo(todo) {
+      const checked = todo.checked ? 'done' : '';
       const eachList = document.createElement("div");
       eachList.setAttribute('class', `each-li ${checked}`);
       eachList.setAttribute('data-key', todo.id);
@@ -49,8 +30,62 @@
       </div>
       <img class="delete" src="/assets/images/icons8-delete-96.png" alt="" srcset="">
     `;
-    lists.append(eachList);
+      lists.append(eachList);
     }
+
+    function completed(e) {
+      const todos = lists.childNodes;
+      let choice = e.target.value;
+      for (let i = 0; i < todos.length; i++) {
+          if (choice === 'completed') {
+            let check = todos[i].nextSibling.firstChild.nextElementSibling;
+            if (!check.classList.contains("show-completed")) {
+              check.style.display = 'none';
+              check.nextElementSibling.style.display = 'none'
+            }else{
+              check.style.display = 'flex';
+              check.nextElementSibling.style.display = 'flex'
+            }
+          }
+          if (choice === 'incomplete') {
+            let check = todos[i].nextSibling.firstChild.nextElementSibling;
+            console.log(todos[i].nextSibling)
+            if (check.classList.contains("show-completed")) {
+              check.style.display = 'none';
+              check.nextElementSibling.style.display = 'none'
+            }else{
+              check.style.display = 'flex';
+              check.nextElementSibling.style.display = 'flex'
+            }
+          }
+          if(choice === 'all'){
+            let check = todos[i].nextSibling.firstChild.nextElementSibling;
+            if (check.classList.contains("show-completed")) {
+              check.style.display = 'flex';
+              check.nextElementSibling.style.display = 'flex'
+            }
+          }
+        }
+    }
+
+    whichTodos.addEventListener('change', completed);
+
+    lists.addEventListener('click', (event) => {
+      // console.log(event.target)
+      const item = event.target;
+      if (item.classList[0] === 'delete') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', () => {
+          todo.remove()
+        })
+      }
+      if (item.classList[0] === 'list-marker-incomplete') {
+        const todo = item.parentElement;
+        item.classList.toggle('list-marker')
+        todo.classList.toggle('show-completed')
+      }
+    })
 
     button.addEventListener('click', (e) => {
       e.preventDefault();
@@ -59,31 +94,9 @@
         addTodo(input);
         addTask.value = '';
         addTask.focus();
-      }else{
+      } else {
         alert("Task can't be empty")
       }
     })
-
-    // lists.addEventListener('click', event => {
-    //   if (event.target.classList.contains('list-marker')) {
-    //     const taskID = event.target.parentElement;
-    //     // event.target.parentElement.firstChild.style.textDecoration = "line-through";
-    //     // listDecor.classList.toggle(".show-red")
-    //     // for (let i = 0; i < listDecor.length; i++){
-    //     //   console.log(listDecor[i])
-    //     // }
-    //     event.target.classList.toggle('list-marker-incomplete');
-    //     // let dataKey = document.querySelector('[data-key]').className;
-    //     // dataKey[0].classList.toggle('show-red');
-    //       // dataKey[i].style.color = "red";
-    // }
-    //   if(event.target.classList.contains('delete')) {
-    //     const taskID = event.target.parentElement;
-    //     taskID.remove();
-    //     // console.log(taskID.id)
-    //   }
-    // });
-
-
   })
-}())
+}());
